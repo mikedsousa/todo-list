04ba8c816dae4155ea427a7cf9b17b774d65bb20
mikedsousa <95877997+mikedsousa@users.noreply.github.com>
2024-01-07 09:24:29 -0300

    att

--- javascript/_AXIOS/js/script.js
+++ javascript/_AXIOS/js/script.js
@@ -1,7 +1,8 @@
 const getData = async () => {
   try {
     const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/users", {
+      "https://jsonplaceholder.typicode.com/users",
+      {
         headers: {
           "Content-Type": "application/json",
         },
@@ -44,3 +45,16 @@ const printData = async () => {
 };
 
 printData();
+
+const form = document.querySelector("#post-form");
+const titleInput = document.querySelector("#title");
+const bodyInput = document.querySelector("#body");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  axios.post("https://jsonplaceholder.typicode.com/posts", {
+    title: titleInput.value,
+    body: bodyInput.value,
+    userId: 1,
+  });
+});
