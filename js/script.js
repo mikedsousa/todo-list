fcaac1295c9856b5de1bf17096d053a1e343a4c9
mikedsousa <95877997+mikedsousa@users.noreply.github.com>
2024-01-08 10:26:25 -0300

    edit task - done

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
+let oldInputValue;
+
 // Funções
 
 const saveTodo = (text) => {
@@ -23,7 +25,7 @@ const saveTodo = (text) => {
   todo.appendChild(doneBtn);
 
   const editBtn = document.createElement("button");
-  editBtn.classList.add("finish-todo");
+  editBtn.classList.add("edit-todo");
   editBtn.innerHTML = '<i class="fas fa-pen"></i>';
   todo.appendChild(editBtn);
 
@@ -44,6 +46,18 @@ const toggleForms = () => {
   todoList.classList.toggle("hide");
 };
 
+const updateTodo = (text) => {
+  const todos = document.querySelectorAll(".todo");
+
+  todos.forEach((todo) => {
+    let todoTitle = todo.querySelector("h3");
+
+    if (todoTitle.innerText === oldInputValue) {
+      todoTitle.innerText = text;
+    }
+  })
+};
+
 // Eventos
 
 todoForm.addEventListener("submit", (e) => {
@@ -60,6 +74,11 @@ todoForm.addEventListener("submit", (e) => {
 document.addEventListener("click", (e) => {
   const targetEl = e.target;
   const parentEl = targetEl.closest("div");
+  let todoTitle;
+
+  if (parentEl && parentEl.querySelector("h3")) {
+    todoTitle = parentEl.querySelector("h3").innerText;
+  }
 
   if (targetEl.classList.contains("finish-todo")) {
     parentEl.classList.toggle("done");
@@ -71,10 +90,26 @@ document.addEventListener("click", (e) => {
 
   if (targetEl.classList.contains("edit-todo")) {
     toggleForms();
+
+    editInput.value = todoTitle;
+    oldInputValue = todoTitle;
   }
 });
 
 cancelEditBtn.addEventListener("click", (e) => {
   e.preventDefault();
+  toggleForms();
+});
+
+editForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const editedInputValue = editInput.value;
+
+  if (editedInputValue) {
+    // atualizar
+    updateTodo(editedInputValue);
+  }
+
   toggleForms();
 });
